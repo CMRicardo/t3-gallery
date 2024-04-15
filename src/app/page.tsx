5bfa181869e42546f0386de186a8ae2e085eec30
CMRicardo <ricardo.corrales.montoya@gmail.com>
2024-04-15 15:13:21 -0600

    Add @vercel/postgres package and update database configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "@/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/8ffe3d7f-c145-4226-9b9c-e2608d8911fd-wu3imb.jpg",
   "https://utfs.io/f/ebc102ff-f1f0-427b-80e0-875fe19a4073-1bmwd.jpg",
@@ -9,13 +11,21 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            <p>{post.name}</p>
+          </div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(
-          (image) => (
-            <div key={image.id} className="w-48">
+          (image, index) => (
+            <div key={image.id + "-" + index} className="w-48">
               <img src={image.url} />
             </div>
           ),
