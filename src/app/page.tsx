76baaa00f99d4bc800fd579ed05726e64992763c
CMRicardo <ricardo.corrales.montoya@gmail.com>
2024-04-15 14:44:07 -0600

    Update README.md, src/app/layout.tsx, src/app/page.tsx, and src/styles/globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
+const mockUrls = [
+  "https://utfs.io/f/8ffe3d7f-c145-4226-9b9c-e2608d8911fd-wu3imb.jpg",
+  "https://utfs.io/f/ebc102ff-f1f0-427b-80e0-875fe19a4073-1bmwd.jpg",
+  "https://utfs.io/f/030a1cc3-8d8d-4666-b0b6-d30f850cf1ef-nlzi21.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(
+          (image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} />
+            </div>
+          ),
+        )}
+      </div>
       Hello (gallery in progress)
     </main>
   );
